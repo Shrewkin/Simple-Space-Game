f90f18a858e60cf53b7c56a2e14daacb7f89c191
Shrewkin <strizhevp@gmail.com>
2018-12-11 17:35:08 -0800

    Day 1 Upload 1
    Added a template for a random planet generator, going to start working on the player ship next and try to get movement for that.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehavior
+{
+    public GameObject prefab;
+    public static GameObject Earth;
+    public static float lastTime;
+
+    void Update()
+    {
+        Earth = GameObject.FindGameObjectWithTag("Earth");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            lastTime = Time.time;
+            Instantiate(prefab, Earth.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Planet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+    //Range For Speed
+    [Range(0, 100)]
+    public float speed = 20;
+    public float alpha;
+    public float distance;
+    public float Rotation;
+    public float currentTime;
+
+    void Start()
+    {
+        Rotation = Random.Range(-2.0f, 2.0f);
+        distance = Random.Range(1.0f, 2.0f);
+        var Size = Random.Range(0.2f, 0.7f);
+        transform.localScale = new Vector3(Size, Size, Size);
+
+        currentTime = Time.time - Generator.lastTime;
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        var X = (distance * Mathf.Cos(currentTime));
+        var Y = (distance * Mathf.Sin(currentTime));
+        var R = (Rotation * Mathf.Cos(currentTime));
+
+        var Earth = Generator.Earth;
+        transform.position = Earth.transform.position + new Vector3(X, R, Y);
+    }
+}
